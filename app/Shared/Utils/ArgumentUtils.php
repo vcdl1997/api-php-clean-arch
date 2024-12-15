3e192aa03963078eb848faf87c52324d183d2365
Vinicius Costa de Lima <vcdl1997@gmail.com>
2024-12-14 22:17:35 -0300

    ci: configuração de sonarqube

--- app/Shared/Utils/ArgumentUtils.php
+++ app/Shared/Utils/ArgumentUtils.php
@@ -5,10 +5,10 @@ namespace App\Shared\Utils;
 class ArgumentUtils
 {
     public static function validateAllByPredicate(array $args, callable $callback): bool {
+        $totalArgs = count($args);
 
         if(empty($totalArgs)) return false;
 
-        $totalArgs = count($args);
         $totalArgsValidated = count(array_filter($args, $callback));
 
         return $totalArgs === $totalArgsValidated;
